fcecc21f7f108ccf89fd967b8c708e1075fa37fe
Gustavo <gaps0729@gmail.com>
2026-04-29 23:47:28 -0500

    docs: mejora valores por defecto en swagger

--- src/Application/DTOs/ProductoCreateDto.cs
+++ src/Application/DTOs/ProductoCreateDto.cs
@@ -4,20 +4,16 @@ namespace Application.DTOs;
 
 public class ProductoCreateDto
 {
+    [Required]
+    [MinLength(3)]
+    public string Nombre { get; set; } = "Mouse Logitech";
 
-[Required]
-[MinLength(3)]
-public string Nombre { get; set; } = "Mouse Logitech";
+    [Required]
+    public string Descripcion { get; set; } = "Mouse inalámbrico";
 
+    [Range(0.01, double.MaxValue)]
+    public decimal Precio { get; set; } = 70000;
 
-[Required]
-public string Descripcion { get; set; } = "Mouse inalámbrico";
-
-
-[Range(0.01, double.MaxValue)]
-public decimal Precio { get; set; } = 20000;
-
-
-[Range(0, int.MaxValue)]
-public int Stock { get; set; } = 15;
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; } = 15;
 }
--- src/Application/DTOs/StockUpdateDto.cs
+++ src/Application/DTOs/StockUpdateDto.cs
@@ -1,11 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 
-
 namespace Application.DTOs;
 
 public class StockUpdateDto
 {
-    
-[Range(1, int.MaxValue)]
-public int Cantidad { get; set; } = 5;
+    [Range(1, int.MaxValue)]
+    public int Cantidad { get; set; } = 5;
 }
